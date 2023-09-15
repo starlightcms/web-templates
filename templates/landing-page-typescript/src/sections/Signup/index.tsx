9800096829e7a61e805afc6ae5daca8e1d51fbb7
Rubens Diniz <rubensadiniz@gmail.com>
2023-09-15 14:07:20 -0300

    Add signup functionality

--- templates/landing-page-typescript/src/sections/Signup/index.tsx
+++ templates/landing-page-typescript/src/sections/Signup/index.tsx
@@ -1,6 +1,8 @@
 import { Button, Col, Form, Row } from "react-bootstrap";
 import { Singleton, Image } from "@starlightcms/next-sdk";
 import { SignupSingleton } from "@/starlight";
+import { FormEvent, useState } from "react";
+import clsx from "clsx";
 
 type SignupProps = {
   singleton: Singleton<SignupSingleton>;
@@ -11,7 +13,32 @@ type SignupProps = {
  * an email input and a "Sign up" button.
  */
 export default function Signup({ singleton }: SignupProps) {
-  // TODO! Fix image size?
+  const [email, setEmail] = useState("");
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [status, setStatus] = useState(0);
+
+  const signUp = async (event: FormEvent) => {
+    setIsSubmitting(true);
+    event.preventDefault();
+
+    const response = await fetch(
+      "https://submit.starlightcms.io/v2/organizations/starlight/workspaces/landing-page-template/forms/signup",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email }),
+      },
+    );
+
+    setIsSubmitting(false);
+    setStatus(response.status);
+
+    if (response.status === 200) setEmail("");
+  };
+
+  // TODO! OPACITY TRANSITION
   return (
     <Row className="bg-brand-400 text-center text-gray-100 mx-0 rounded-4 overflow-hidden d-lg-flex flex-lg-row">
       <Col className="p-0" sm={12} lg={4}>
@@ -29,15 +56,41 @@ export default function Signup({ singleton }: SignupProps) {
         <h2 className="mb-2 fw-bold">{singleton.data.title}</h2>
         <p className="lh-lg m-0">{singleton.data.description}</p>
 
-        <div className="d-flex flex-column justify-content-center align-items-stretch w-100 my-4 mx-0 gap-2 flex-lg-row">
+        <Form
+          onSubmit={signUp}
+          className="d-flex flex-column justify-content-center align-items-stretch w-100 my-4 mx-0 gap-2 flex-lg-row"
+        >
           <Form.Control
             placeholder={singleton.data.email_placeholder}
             className="p-3 w-100"
+            value={email}
+            onChange={(event) => setEmail(event.target.value)}
+            disabled={isSubmitting}
+            type="email"
+            id="email"
           />
-          <Button className="py-3 px-5 bg-brand-100 border-brand-100 fw-bold text-brand-500 flex-shrink-0">
+          <Button
+            type="submit"
+            className="py-3 px-5 bg-brand-100 border-brand-100 fw-bold text-brand-500 flex-shrink-0"
+            disabled={isSubmitting}
+          >
             {singleton.data.button_label}
           </Button>
-        </div>
+        </Form>
+        {status !== 0 ? (
+          <p
+            className={clsx(
+              "fs-6 mb-3 fw-bold",
+              status === 200 ? "text-brand-50" : "text-danger",
+            )}
+          >
+            {status === 200
+              ? "Cadastro realizado com sucesso. Obrigado!"
+              : "Erro ao tentar realizar o cadastro, tente novamente em alguns minutos."}
+          </p>
+        ) : (
+          <></>
+        )}
         <p className="fs-6 mb-0">{singleton.data.footnote}</p>
       </Col>
     </Row>
